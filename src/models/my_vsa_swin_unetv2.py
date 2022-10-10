a6c6c07e3048024a2af4690d26a4579f215b7b57
ron8520 <ronming@outlook.com>
2022-10-10 23:59:59 +1100

    modify VSA pretrain path

--- src/models/my_vsa_swin_unetv2.py
+++ src/models/my_vsa_swin_unetv2.py
@@ -6,7 +6,7 @@ class MyVSASwinUnetV2(nn.Module):
   def __init__(self):
     super(MyVSASwinUnetV2, self).__init__()
     self.backbone = VSASwinUnet()
-    self.backbone.load_from('/content/drive/MyDrive/transformer-experiment/SwinUnet/pretrained_ckpt/swin_tiny_patch4_window7_224.pth')
+    self.backbone.load_from('./SwinUnet/pretrained_ckpt/swin_tiny_patch4_window7_224.pth')
 
   def forward(self, x, batch_positions=None):
     out = self.backbone(x, batch_positions)
