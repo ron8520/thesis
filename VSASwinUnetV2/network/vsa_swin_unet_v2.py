5a79f135d52062a1c485e702a5e38c25864ecd55
ron8520 <ronming@outlook.com>
2022-10-12 17:47:42 +1100

    commit out torch.jit.ignore

--- VSASwinUnetV2/network/vsa_swin_unet_v2.py
+++ VSASwinUnetV2/network/vsa_swin_unet_v2.py
@@ -897,13 +897,13 @@ class SwinTransformerSys(nn.Module):
             except AttributeError:
                 pass
 
-    @torch.jit.ignore
-    def no_weight_decay(self):
-        return {'absolute_pos_embed'}
-
-    @torch.jit.ignore
-    def no_weight_decay_keywords(self):
-        return {'relative_position_bias_table'}
+    # @torch.jit.ignore
+    # def no_weight_decay(self):
+    #     return {'absolute_pos_embed'}
+    #
+    # @torch.jit.ignore
+    # def no_weight_decay_keywords(self):
+    #     return {'relative_position_bias_table'}
 
     # Encoder and Bottleneck
     def forward_features(self, x):
