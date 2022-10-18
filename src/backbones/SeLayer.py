741001b72a0f27ae96a42ef3b17733257f8a4cd2
ron8520 <ronming@outlook.com>
2022-10-18 21:59:59 +1100

    Using gelu replace relu in SElayer

--- src/backbones/SeLayer.py
+++ src/backbones/SeLayer.py
@@ -6,7 +6,7 @@ class SELayer(nn.Module):
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.fc = nn.Sequential(
             nn.Linear(channel, channel // reduction, bias=False),
-            nn.ReLU(inplace=True),
+            nn.GELU(),
             nn.Linear(channel // reduction, channel, bias=False),
             nn.Sigmoid()
         )
