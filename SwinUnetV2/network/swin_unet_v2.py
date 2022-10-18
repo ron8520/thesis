4de31172ba35149f89a116ef4092a79588f2d30c
ron8520 <ronming@outlook.com>
2022-10-18 21:08:09 +1100

    modify skip connection

--- SwinUnetV2/network/swin_unet_v2.py
+++ SwinUnetV2/network/swin_unet_v2.py
@@ -851,10 +851,10 @@ class SwinTransformerSys(nn.Module):
 
         # Reshape back to 5 dims 
         for i, elements in enumerate(x_downsample):
-            x_downsample[i] = rearrange(elements, 'b (h w) c -> b c h w',
+            x_downsample[i] = rearrange(x_downsample[i], 'b (h w) c -> b c h w',
                                         h=self.features_sizes[i], w=self.features_sizes[i])
-            x_downsample[i] = self.se_list[i](elements)
-            x_downsample[i] = rearrange(elements, '(b t) c h w -> b t c h w', b=B, t=T)
+            x_downsample[i] = self.se_list[i](x_downsample[i])
+            x_downsample[i] = rearrange(x_downsample[i], '(b t) c h w -> b t c h w', b=B, t=T)
             x_downsample[i] = self.temporal_aggregator(x_downsample[i], pad_mask=pad_mask, attn_mask=att)
             x_downsample[i] = rearrange(x_downsample[i], 'b c h w -> b (h w) c')
 
