e5a9f3c177894333a46476213ab76a7ba0d88a56
ron8520 <ronming@outlook.com>
2022-10-20 00:19:38 +1100

    pad_mask unsequeeze)

--- src/backbones/ltae.py
+++ src/backbones/ltae.py
@@ -197,7 +197,7 @@ class MultiHeadAttention(nn.Module):
         q = q.unsqueeze(1) * self.scale
         attn = q @ k.transpose(-2, -1)
 
-        attn = attn.masked_fill(pad_mask, -1e3)
+        attn = attn.masked_fill(pad_mask.unsqueeze(1), -1e3)
         attn = self.softmax(attn)
         attn = self.dropout(attn)
 
