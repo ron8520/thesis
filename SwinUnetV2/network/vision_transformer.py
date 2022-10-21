e04a3805b50803a070efa0cd3dae8bc7296323f7
ron8520 <ronming@outlook.com>
2022-10-21 20:23:04 +1100

    conv stride change to 2 in patch_emb

--- SwinUnetV2/network/vision_transformer.py
+++ SwinUnetV2/network/vision_transformer.py
@@ -28,13 +28,13 @@ class SwinUnet(nn.Module):
 
         self.swin_unet = SwinTransformerSys(
                                 img_size=128,
-                                patch_size=4,
+                                patch_size=2,
                                 in_chans=64,
                                 num_classes=20,
                                 embed_dim=96,
                                 depths=[2, 2, 6, 2],
                                 num_heads=[3, 6, 12, 24],
-                                window_size=4,
+                                window_size=8,
                                 mlp_ratio=4.,
                                 qkv_bias=True,
                                 qk_scale=None,
