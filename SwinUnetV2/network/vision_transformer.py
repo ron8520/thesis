1903d950cadb8e2b3c31f94b4b932fdf78e6ebe7
ron8520 <ronming@outlook.com>
2022-10-22 17:07:04 +1100

    adding MBconv block at each stage

--- SwinUnetV2/network/vision_transformer.py
+++ SwinUnetV2/network/vision_transformer.py
@@ -32,7 +32,7 @@ class SwinUnet(nn.Module):
                                 in_chans=64,
                                 num_classes=20,
                                 embed_dim=96,
-                                depths=[2, 2, 6, 2],
+                                depths=[3, 3, 9, 3],
                                 num_heads=[3, 6, 12, 24],
                                 window_size=4,
                                 mlp_ratio=4.,
--- src/backbones/componets.py
+++ src/backbones/componets.py
@@ -1,13 +1,64 @@
 import torch.nn as nn
+from einops import rearrange
+from einops.layers.chainer import Reduce, Rearrange
 
+
+class SqueezeExcitation(nn.Module):
+    def __init__(self, dim, shrinkage_rate = 0.25):
+        super().__init__()
+        hidden_dim = int(dim * shrinkage_rate)
+
+        self.gate = nn.Sequential(
+            Reduce('b c h w -> b c', 'mean'),
+            nn.Linear(dim, hidden_dim, bias = False),
+            nn.SiLU(),
+            nn.Linear(hidden_dim, dim, bias = False),
+            nn.Sigmoid(),
+            Rearrange('b c -> b c 1 1')
+        )
+
+    def forward(self, x):
+        return x * self.gate(x)
+
+class MBConv(nn.Module):
+    def __init__(self, dim_in,
+                 input_resolution,
+                 dim_out,
+                 expansion_rate=4,
+                 shrinkage_rate=0.25,
+                 dropout=0.):
+        super().__init__()
+        hidden_dim = int(expansion_rate * dim_out)
+        self.input_resolution = input_resolution
+        self.net = nn.Sequential(
+            nn.Conv2d(dim_in, hidden_dim, 1),
+            nn.BatchNorm2d(hidden_dim),
+            nn.GELU(),
+            nn.Conv2d(hidden_dim, hidden_dim, 3, stride=1, padding=1, groups=hidden_dim),
+            nn.BatchNorm2d(hidden_dim),
+            nn.GELU(),
+            SqueezeExcitation(hidden_dim, shrinkage_rate=shrinkage_rate),
+            nn.Conv2d(hidden_dim, dim_out, 1),
+            nn.BatchNorm2d(dim_out)
+        )
+        self.drop = nn.Dropout(dropout)
+
+    def forward(self, x):
+        out = rearrange(x, 'b (h w) c -> b c h w', h=self.input_resolution[0], w=self.input_resolution[1])
+        out = self.net(out)
+        out = self.drop(out)
+        out = rearrange(out, 'b c h w -> b (h w) c')
+        return x + out
 
 class GroupNorm(nn.GroupNorm):
     """
     Group Normalization with 1 group.
     Input: tensor in shape [B, C, H, W]
     """
+
     def __init__(self, num_channels, **kwargs):
         super().__init__(1, num_channels, **kwargs)
+
 
 class Feature_aliasing(nn.Module):
 
@@ -23,6 +74,7 @@ class Feature_aliasing(nn.Module):
         x = self.norm(x)
         return self.act(x)
 
+
 class Feature_reduce(nn.Module):
 
     def __init__(self, in_channels, out_channels):
@@ -35,6 +87,7 @@ class Feature_reduce(nn.Module):
         x = self.conv(x)
         x = self.norm(x)
         return self.act(x)
+
 
 class Prediction_head(nn.Module):
 
