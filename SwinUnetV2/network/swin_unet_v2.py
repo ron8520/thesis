07c23d90086c41cf327519c528551df6ef285a1f
ron8520 <ronming@outlook.com>
2022-10-24 00:10:03 +1100

    reduction SE is 0.25

--- SwinUnetV2/network/swin_unet_v2.py
+++ SwinUnetV2/network/swin_unet_v2.py
@@ -650,7 +650,6 @@ class PatchEmbed(nn.Module):
         assert H == self.img_size[0] and W == self.img_size[1], \
             f"Input image size ({H}*{W}) doesn't match model ({self.img_size[0]}*{self.img_size[1]})."
         x = self.proj(x).flatten(2).transpose(1, 2)  # B Ph*Pw C
-        print(x.shape)
         if self.norm is not None:
             x = self.norm(x)
         return x
