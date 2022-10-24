58a1e0ccd9c4dfd32db8a2e9ba0df875abcd8671
ron8520 <ronming@outlook.com>
2022-10-24 21:09:41 +1100

    print out conv shape

--- src/backbones/componets.py
+++ src/backbones/componets.py
@@ -291,6 +291,7 @@ class MultiWindowAttention(nn.Module):
         q, k, v = qkv[0], qkv[1], qkv[2]  # make torchscript happy (cannot use tensor as tuple)
 
         print(f"print out v shape: {v.shape}")
+        print(f"print out conv out shape {conv_out.shape}")
 
         # Sentinel-1a
         qkv_a = self.qkv_a(s1a).reshape(Ba_, Na, 3, self.num_heads, C // self.num_heads).permute(2, 0, 3, 1, 4)
